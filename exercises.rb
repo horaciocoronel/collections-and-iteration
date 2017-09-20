773dd8cd398bbe7ab46e58260ae589276557b8c9
horaciocoronel <arielc@gmail.com>
2017-09-20 17:15:16 -0400

    Exercise 0 complete

---
+++ exercises.rb
@@ -0,0 +1,35 @@
+### Exercise 0 ###
+
+############### CREATE ARRAYS ################
+fav_colours = ["red", "blue", "orange", "yellow", "purple"]
+age = [33, 29, 16, 20, 25]
+coin = []
+artists = ["U2", "Rolling Stones", "Armin Van Buuren"]
+fav_colours_symbols = [:red, :blue, :orange, :yellow, :purple]
+
+################ CREATE HASHES #################
+
+words_definition = {
+  :minute => "Measure of time",
+  :cup => "Container that holds liquids",
+  :dollar => "Currency"
+}
+
+movies = {
+  :it => "2017",
+  :titanic => "1999",
+  :alien => "2016"
+}
+
+cities = {
+  :toronto => "2,615,009",
+  :paris => "2,241,346",
+  :rome => "2,869,461"
+}
+
+family = {
+  :maria => "30",
+  :laura => "20",
+  :carl => "10",
+  :luis => "15"
+}
