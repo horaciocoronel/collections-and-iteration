346b60f354f62b53b38998ac61fd0d9bfd62aa58
horaciocoronel <arielc@gmail.com>
2017-09-25 00:02:19 -0400

    Exercise 12.2

--- exercise12.rb
+++ exercise12.rb
@@ -7,3 +7,18 @@ new_merged_array << fav_artists
 new_merged_array.flatten!
 
 new_merged_array.sort!
+
+#Using the array of ages and the array of favourite artists, output a message for each pair of items. For example:
+
+artists = ["U2", "Pink Floyd", "Armin Van Buuren", "Rolling Stones", "Coldplay"]
+age = [33, 29, 16, 20, 25]
+
+new_merged_array_age = {}
+
+artists.each_with_index do |value, index|
+  new_merged_array_age[value] = age[index]
+end
+
+new_merged_array_age.each do | k, v|
+  puts "I <3 #{k} #{v}"
+end
